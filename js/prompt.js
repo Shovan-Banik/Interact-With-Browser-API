3cffdbeb556ec116d679e57cb918dd85dd1ffba9
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-10 04:05:30 +0600

    alert,confirm, and prompt method exploring

---
+++ js/prompt.js
@@ -0,0 +1,11 @@
+const biriAlert=()=>{
+    alert('Bujhteci na khamo kina');
+}
+const biriConfirm=()=>{
+   const result= confirm('khamo aitaci');
+   console.log(result);
+}
+const biriNumbers=()=>{
+    const output=prompt('koyta ace tr kace???');
+    console.log(`beshi nai matro ${output} ta ace`);
+}
