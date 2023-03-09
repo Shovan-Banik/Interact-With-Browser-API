45802ad060395dc78538390f31fb331fc4530697
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-10 05:29:11 +0600

    localStorage set,get and remove method

---
+++ js/local.js
@@ -0,0 +1,8 @@
+const input=localStorage.getItem('name');
+const input1=localStorage.getItem('address');
+
+console.log(input,input1);
+
+const setAge=()=>{
+    localStorage.setItem('age','27');
+}
